5012f64e27d82f7a5330073987f9495a93c8ce02
liwenguang <1254755805@qq.com>
2018-12-01 18:22:58 +0800

    删除旧的通知记录

--- test/src/test/java/com/zed/dingtalk/service/msgnotify/SendWorkNofifyServiceTest.java
+++ test/src/test/java/com/zed/dingtalk/service/msgnotify/SendWorkNofifyServiceTest.java
@@ -1,7 +1,9 @@
 package com.zed.dingtalk.service.msgnotify;
 
 import com.zed.dingtalk.DingTalkAppTest;
-import com.zed.dingtalk.service.msgnotify.sendnotify.*;
+import com.zed.dingtalk.service.msgnotify.sendnotify.SendWorkAsyncSendRequest;
+import com.zed.dingtalk.service.msgnotify.sendnotify.SendWorkNotifyService;
+import com.zed.dingtalk.service.msgnotify.sendnotify.SendWorkResponse;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -15,22 +17,6 @@ import java.util.Collections;
 public class SendWorkNofifyServiceTest extends DingTalkAppTest {
     @Autowired
     private SendWorkNotifyService sendWorkNotifyService;
-
-    @Test
-    public void asyncSend() {
-        AsyncSendRequest asyncSendRequest = new AsyncSendRequest();
-        asyncSendRequest.setSession(accessToken);
-        asyncSendRequest.setMsgtype("oa");
-        asyncSendRequest.setMsgcontent("{\"message_url\": \"http://dingtalk.com\",\"head\": {\"bgcolor\": \"FFBBBBBB\",\"text\": \"头部标题\"},\"body\": {\"title\": \"正文标题\",\"form\": [{\"key\": \"姓名:\",\"value\": \"张三\"},{\"key\": \"爱好:\",\"value\": \"打球、听音乐\"}],\"rich\": {\"num\": \"15.6\",\"unit\": \"元\"},\"content\": \"大段文本大段文本大段文本大段文本大段文本大段文本大段文本大段文本大段文本大段文本大段文本大段文本\",\"image\": \"@lADOADmaWMzazQKA\",\"file_count\": \"3\",\"author\": \"李四 \"}}");
-        asyncSendRequest.setUserid_list(Collections.singletonList("manager2090"));
-        AsyncSendResponse asyncSendResponse = sendWorkNotifyService.asyncSend(asyncSendRequest);
-        asyncSendResponse.getRequestId();
-        if (asyncSendResponse.isSuc()) {
-            asyncSendResponse.getSucDetail();
-        } else {
-            asyncSendResponse.getFailDetail();
-        }
-    }
 
     @Test
     public void asyncSend2() {
